112af9492562cbb64c5cbfc9949a54beb1f5cf45
Bastien DHIVER <epitech@bastn.fr>
2016-04-03 14:59:47 +0200

    DO a PTRACE_DETACH when SIGINT

--- src/signals.c
+++ src/signals.c
@@ -5,19 +5,22 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Sat Apr 02 22:31:08 2016 Bastien DHIVER
-** Last update Sun Apr 03 00:45:13 2016 Bastien DHIVER
+** Last update Sun Apr 03 14:55:19 2016 Bastien DHIVER
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/ptrace.h>
 #include "strace.h"
 
 void	get_sigint(int sig)
 {
   (void)sig;
-  fprintf(stderr, "Process %d detached\n", g_pid);
-  fprintf(stderr, " <detached ...>\n");
-  /*if (ptrace(PTRACE_DETACH, pid, NULL, NULL) == -1)*/
-    /*return (fprintf(stderr, strerror(errno)), 1);*/
+  if (ptrace(PTRACE_DETACH, g_pid, NULL, NULL) == -1)
+    print(strerror(errno));
+  print("Process %d detached\n", g_pid);
+  print(" <detached ...>\n");
   exit(0);
 }
