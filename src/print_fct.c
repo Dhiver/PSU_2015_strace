5a018628c71c5fa2250acb07d74d10524655e5d5
Bastien DHIVER <epitech@bastn.fr>
2016-04-10 18:13:35 +0200

    Show non-printable char in octal

--- src/print_fct.c
+++ src/print_fct.c
@@ -5,11 +5,12 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Tue Apr 05 21:11:06 2016 Bastien DHIVER
-** Last update Sun Apr 10 16:00:53 2016 Bastien DHIVER
+** Last update Sun Apr 10 18:03:50 2016 Bastien DHIVER
 */
 
 #include <sys/ptrace.h>
 #include <string.h>
+#include <ctype.h>
 #include "strace.h"
 
 int	print_unkn(long_stuff value, t_bool details, t_bool is_child)
@@ -32,6 +33,7 @@ int	print_str(long_stuff value, t_bool details, t_bool is_child)
   int	written;
   long	data;
   int	i;
+  char	c;
 
   written = 0;
   i = 0;
@@ -39,9 +41,10 @@ int	print_str(long_stuff value, t_bool details, t_bool is_child)
     {
       data = ptrace(PTRACE_PEEKDATA, g_pid, value + i * sizeof(char), NULL);
       written += print("\"");
-      while ((char)data != 0 && written != MAX_PRINT_CHAR && (i += 1))
+      c = (char)data;
+      while (c != 0 && written != MAX_PRINT_CHAR && (i += 1))
 	{
-	  written += print("%c", (char)data);
+	  written += isprint(c) ? print("%c", c) : print("\\%o", c);
 	  data = ptrace(PTRACE_PEEKDATA, g_pid, value + i * sizeof(char), NULL);
 	}
       written += print("\"");
